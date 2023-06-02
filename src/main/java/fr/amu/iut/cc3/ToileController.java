137f351c1d2e07c9fcc5d7c68f48798d9d12a92d
a22021019 <clement@antranet.fr>
2023-06-02 14:16:06 +0200

    14:16

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -17,6 +17,8 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -31,9 +33,71 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
-
+    @FXML
+    private TextField Comp1;
+    @FXML
+    private TextField Comp2;
+    @FXML
+    private TextField Comp3;
+    @FXML
+    private TextField Comp4;
+    @FXML
+    private TextField Comp5;
+    @FXML
+    private TextField Comp6;
+    @FXML
+    private HBox scene;
+    @FXML
+    private Pane PToile;
+    private Circle PComp1 = new Circle();
+    private Circle PComp2 = new Circle();
+    private Circle PComp3 = new Circle();
+    private Circle PComp4 = new Circle();
+    private Circle PComp5 = new Circle();
+    private Circle PComp6 = new Circle();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void TracerPoints() {
+        PComp1.setCenterX(getXRadarChart(Double.parseDouble(Comp1.getText()), 1));
+        PComp1.setCenterY(getYRadarChart(Double.parseDouble(Comp1.getText()), 1));
+        PComp2.setCenterX(getXRadarChart(Double.parseDouble(Comp2.getText()), 2));
+        PComp2.setCenterY(getYRadarChart(Double.parseDouble(Comp2.getText()), 2));
+        PComp3.setCenterX(getXRadarChart(Double.parseDouble(Comp3.getText()), 3));
+        PComp3.setCenterY(getYRadarChart(Double.parseDouble(Comp3.getText()), 3));
+        PComp4.setCenterX(getXRadarChart(Double.parseDouble(Comp4.getText()), 4));
+        PComp4.setCenterY(getYRadarChart(Double.parseDouble(Comp4.getText()), 4));
+        PComp5.setCenterX(getXRadarChart(Double.parseDouble(Comp5.getText()), 5));
+        PComp5.setCenterY(getYRadarChart(Double.parseDouble(Comp5.getText()), 5));
+        PComp6.setCenterX(getXRadarChart(Double.parseDouble(Comp6.getText()), 6));
+        PComp6.setCenterY(getYRadarChart(Double.parseDouble(Comp6.getText()), 6));
+
+        PComp1.setRadius(5);
+        PComp2.setRadius(5);
+        PComp3.setRadius(5);
+        PComp4.setRadius(5);
+        PComp5.setRadius(5);
+        PComp6.setRadius(5);
+        PComp1.setVisible(false);
+        PComp2.setVisible(false);
+        PComp3.setVisible(false);
+        PComp4.setVisible(false);
+        PComp5.setVisible(false);
+        PComp6.setVisible(false);
+        
+
+        PToile.getChildren().addAll(PComp1, PComp2, PComp3, PComp4, PComp5, PComp6);
+    }
+    
+    public void ViderPoints() {
+        PComp1.setVisible(false);
+        PComp2.setVisible(false);
+        PComp3.setVisible(false);
+        PComp4.setVisible(false);
+        PComp5.setVisible(false);
+        PComp6.setVisible(false);
     }
 
     int getXRadarChart(double value, int axe ){
